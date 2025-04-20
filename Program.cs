32915f161c99434fbb2bd8e7d84890b33cd43100
Sangeetha Joseph <sangeethajoseph76@gmail.com>
2025-04-20 21:31:02 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEven_Checker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 10;
+            int num2 = 2;
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+            Console.ReadLine();
+        }
+    }
+}
